4bbbeb96500c048e381d568ef205ffb1fd26387d
YanRun <yanrun2007@hotmail.com>
2017-01-06 16:20:54 +0800

    update

--- MyApplication/app/src/main/java/com/example/jinfei/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/jinfei/myapplication/MainActivity.java
@@ -8,15 +8,18 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.jinfei.myapplication.bean.ResponseBean;
 import com.example.jinfei.myapplication.client.CommonOkHttpClient;
 import com.example.jinfei.myapplication.listener.DisposeDataHandler;
 import com.example.jinfei.myapplication.listener.DisposeDataListener;
-import com.example.jinfei.myapplication.request.RequestParams;
 
-import java.util.HashMap;
+import java.io.IOException;
 import java.util.List;
-import java.util.Map;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends BaseActivity {
 
@@ -95,22 +98,73 @@ public class MainActivity extends BaseActivity {
         ActivityController.removeActivity(this);
     }
 
-    private void coolPost() {
-        Map<String, Object> paramMap = new HashMap<>();
-        paramMap.put("mb", "19811230100");
-        paramMap.put("pwd", "999999q");
-        RequestParams params = new RequestParams(paramMap);
-        CommonOkHttpClient.post("http://mail.docmail.cn/auth/login", params, new DisposeDataHandler(new DisposeDataListener() {
-            @Override
-            public void onSuccess(Object response) {
-                tvContent.setText(response.toString());
-            }
+    OkHttpClient client = new OkHttpClient();
+    String post(String url) throws IOException {
+        RequestBody formBody = new FormBody.Builder()
+                .add("key", "07dd87c07e42408b87d2642970c71be0")
+                .add("info", "明天去上海的航班")
+                .add("loc", "北京市中关村")
+                .add("userid", "12345678")
+                .build();
 
+        Request request = new Request.Builder()
+                .url(url)
+                .post(formBody)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        if (response.isSuccessful()) {
+            return response.body().string();
+        } else {
+            throw new IOException("Unexpected code " + response);
+        }
+    }
+
+    private void coolPost() {
+//        Map<String, Object> paramMap = new HashMap<>();
+//        paramMap.put("key", "07dd87c07e42408b87d2642970c71be0");
+//        paramMap.put("info", "今天天气怎么样");
+//        paramMap.put("loc", "北京市中关村");
+//        paramMap.put("userid", "12345678");
+//        RequestParams params = new RequestParams(paramMap);
+//        CommonOkHttpClient.post("http://www.tuling123.com/openapi/api", params, new DisposeDataHandler(new DisposeDataListener() {
+//            @Override
+//            public void onSuccess(Object response) {
+//                tvContent.setText(response.toString());
+//            }
+//
+//            @Override
+//            public void onFailure(Object error) {
+//                tvContent.setText(error.toString());
+//            }
+//        }, ResponseBean.class));
+//        CommonOkHttpClient.get("http://www.tuling123.com/openapi/api", params, new DisposeDataHandler(new DisposeDataListener() {
+//            @Override
+//            public void onSuccess(Object response) {
+//                tvContent.setText("1");
+//            }
+//
+//            @Override
+//            public void onFailure(Object error) {
+//                tvContent.setText("2");
+//            }
+//        }));
+        new Thread(new Runnable() {
             @Override
-            public void onFailure(Object error) {
-                tvContent.setText(error.toString());
+            public void run() {
+                try {
+                    final String responseStr = post("http://www.tuling123.com/openapi/api");
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            tvContent.setText(responseStr);
+                        }
+                    });
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-        }, ResponseBean.class));
+        }).start();
     }
 
 }
